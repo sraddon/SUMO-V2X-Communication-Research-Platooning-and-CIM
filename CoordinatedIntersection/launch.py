bd2238cdff10dc7a94b4df17586b28af0f0fb239
Sam <sraddon@gmail.com>
2019-02-01 16:43:08 +0000

    Polish Basic Platooning at Intersections
    Removed hard-coded lane check
    Updated car attributes when platooning

--- CoordinatedIntersection/launch.py
+++ CoordinatedIntersection/launch.py
@@ -7,22 +7,29 @@ class SimulationManager():
     def __init__(self):
         self.stoppedCarsByLane = dict()
         self.activePlatoons = dict()
+        self.laneNodeConnections = dict()
+        for lane in traci.lane.getIDList():
+            self.laneNodeConnections[lane] = traci.lane.getLinkNumber(lane)
 
     def createPlatoon(self, vehicleIDs):
+        # A function to create a platoon of vehicles
+        # out of a given list of vehicles
         for vehicle in vehicleIDs:
             traci.vehicle.setTau(vehicle, 0)
-            traci.vehicle.setSpeedDev(vehicle, 0)
+            traci.vehicle.setSpeedFactor(vehicle, 1)
             traci.vehicle.setMinGap(vehicle, 1)
             traci.vehicle.setImperfection(vehicle, 0)
 
     def disbandPlatoon(self, vehicleIDs):
+        # A function to remove a list of vehicles from their respective platoon
         for vehicle in vehicleIDs:
             traci.vehicle.setTau(vehicle, 1)
-            traci.vehicle.setSpeedDev(vehicle, 0.1)
+            traci.vehicle.setSpeedFactor(vehicle, 0.9)
             traci.vehicle.setMinGap(vehicle, 2.5)
             traci.vehicle.setImperfection(vehicle, 0.5)
 
     def handleSimulationStep(self):
+        # Handles a single step of the simulation
         idList = traci.vehicle.getIDList()
         processedLanes = set()
         for vehicleID in idList:
@@ -37,7 +44,7 @@ class SimulationManager():
             else:
                 # If a lane with stopped cars has not already
                 # been checked and it is of an appropriate type
-                for lane in [l for l in self.stoppedCarsByLane.keys() if (l not in processedLanes and "Traffic" in l)]:
+                for lane in [l for l in self.stoppedCarsByLane.keys() if (l not in processedLanes and self.laneNodeConnections[l] > 1)]:
                     if vehicleID in self.stoppedCarsByLane[lane]:
                         self.createPlatoon(self.stoppedCarsByLane[lane])
                         processedLanes.add(lane)
@@ -46,14 +53,13 @@ class SimulationManager():
 
 # Check SUMO has been set up properly
 sumoBinary = checkBinary("sumo-gui")
-sumoCmd = [sumoBinary, "-c",
-           "CoordIntersection.sumocfg", "--step-length", "0.1"]
 
 # Start Simulation and step through
-traci.start(sumoCmd)
+traci.start([sumoBinary, "-c", "CoordIntersection.sumocfg",
+             "--step-length", "0.1"])
 step = 0
 manager = SimulationManager()
-while step < 50000:
+while step < 5000:
     manager.handleSimulationStep()
     traci.simulationStep()
     step += 1
