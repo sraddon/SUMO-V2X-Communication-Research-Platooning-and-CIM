f3dbfed0c32be60b66c8f11bacae20a6e9b88d5f
Sam <sraddon@gmail.com>
2019-02-06 14:25:40 +0000

    Add scenario 3 (no traffic lights on main intersection)

---
+++ Scenario 3 - CoordinatedIntersection - V2X controlled/Launch-Scenario-3.py
@@ -0,0 +1,28 @@
+import logging
+import os
+import sys
+import traci
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from sumolib import checkBinary
+from src.simulationmanager import SimulationManager
+
+
+# Check SUMO has been set up properly
+sumoBinary = checkBinary("sumo-gui")
+
+# Set up logger
+logging.basicConfig(format='%(asctime)s %(message)s')
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+# Start Simulation and step through
+traci.start([sumoBinary, "-c", "CoordIntersection-FullV2X.sumocfg",
+             "--step-length", "0.1", "--collision.action", "none"])
+step = 0
+manager = SimulationManager()
+while step < 5000:
+    manager.handleSimulationStep()
+    traci.simulationStep()
+    step += 1
+
+traci.close()
