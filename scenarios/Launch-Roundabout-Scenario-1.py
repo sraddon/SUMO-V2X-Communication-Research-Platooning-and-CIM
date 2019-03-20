4b529a92b79deb1ddbf462d18117a4738935a393
Sam <sraddon@gmail.com>
2019-03-20 13:47:37 +0000

    Adding roundabout scenarios
    Adding roundabout scenarios, also made minor adjustment to BWT map

---
+++ scenarios/Launch-Roundabout-Scenario-1.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import traci
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from src.simlib import setUpSimulation
+
+setUpSimulation("maps\A13NorthCircularRoundabout/A13NorthCircularRoundabout.sumocfg", 2)
+step = 0
+while step < 5000:
+    traci.simulationStep()
+    step += 1
+
+traci.close()
